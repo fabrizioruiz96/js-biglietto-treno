1009e9ffaa2e2a2212fa791d4e9e26f9f2fe5d7f
fabrizioruiz96 <fabrizioruizzo@gmail.com>
2024-12-11 13:19:53 +0100

    fix: code optimization

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
-const kmTrip = prompt("Quanti chilometri devi percorrere?")
-const agePassenger = prompt("Quanti anni hai?")
+const km = parseFloat(prompt(`Quanti chilometri devi percorrere?`));
+const age = parseInt(prompt(`Quanti anni hai?`))
 
-if ((isNaN(kmTrip) || isNaN(agePassenger)) || (kmTrip==="" || agePassenger==="")){
-    console.log("Inserisci dei numeri validi per favore, ricarica")
-} else if (agePassenger < 18){
-    const priceTrip = kmTrip * 0.21
-    const priceForMinor = priceTrip - ((priceTrip * 20) / 100);
-    const priceInEuro = `€${priceForMinor.toFixed(2).replace("." , ",")}`;
-    console.log(`Il tuo viaggio per ${kmTrip} km costerà ${priceInEuro} con uno sconto applicato del 20%`);
-} else if (agePassenger > 65){
-    const priceTrip = kmTrip * 0.21
-    const priceForMajor = priceTrip - ((priceTrip * 40) / 100);
-    const priceInEuro = `€${priceForMajor.toFixed(2).replace("." , ",")}`;
-    console.log(`Il tuo viaggio per ${kmTrip} km costerà ${priceInEuro} con uno sconto applicato del 40%`);
-} else {
-    const priceTrip = kmTrip * 0.21
+if (isNaN(km) || isNaN(age) || !km || !age){
+    console.log(`Inserisci dei numeri validi per favore, e ricarica!`);
+} else{
+    const pricePerKm = 0.21;
+    let priceTrip = km * pricePerKm;
+    let discount = 0;
+    
+    if (age < 18){
+        discount = 0.2;
+    } else if (age > 65){
+        discount = 0.4;
+    }
+
+    priceTrip *= (1 - discount);
     const priceInEuro = `€${priceTrip.toFixed(2).replace("." , ",")}`;
-    console.log(` Il tuo viaggio per ${kmTrip} km costerà ${priceInEuro}`)
+
+    if (discount > 0){
+        console.log(`Il tuo viaggio per ${km} km costerà ${priceInEuro} con uno sconto applicato del ${discount * 100}%!`)
+    } else {
+        console.log(`Il tuo viaggio per ${km} costerà ${priceInEuro}!`)
+    }
 }
